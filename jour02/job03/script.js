cb68065075030f76d9827be59d6296075ea19371
Enzo-Martolini <enzo.martolini@laplateforme.io>
2024-03-26 10:58:19 +0100

    job03

---
+++ jour02/job03/script.js
@@ -0,0 +1,8 @@
+let compteur = 0;
+
+function addOne() {
+    compteur++;
+    document.getElementById("compteur").textContent = compteur;
+}
+
+document.getElementById("button").addEventListener("click", function () { addOne()});
