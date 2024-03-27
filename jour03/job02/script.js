a795ceabebe533c0f4d98c8326292eb69afe7628
Enzo-Martolini <enzo.martolini@laplateforme.io>
2024-03-27 11:51:18 +0100

    Debut job02

---
+++ jour03/job02/script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var divs = document.querySelectorAll('.container-image-first > div');
+    var divArray = Array.from(divs);
+    console.log(divArray);
+});
