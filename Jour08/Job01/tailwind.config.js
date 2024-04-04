7a7fcfe0a1533e794ea4abbd164ee9340c897680
Enzo-Martolini <enzo.martolini@laplateforme.io>
2024-04-04 14:51:26 +0200

    Jour 04

---
+++ Jour08/Job01/tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.js
+
+module.exports = {
+  // Autres configurations Tailwind ici...
+
+  // Plugins Tailwind
+  plugins: [
+    // Autres plugins...
+    require('@tailwindcss/forms'),
+  ],
+};
